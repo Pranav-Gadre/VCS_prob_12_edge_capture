cc367bdc27e3149185a041a698b00b621f7c9dac
Cb_cat <leapofleopard@gmail.com>
2024-02-25 21:58:28 +0530

    Commit 0: created the file.

---
+++ edge_capture.v
@@ -0,0 +1,14 @@
+module edge_capture (
+	input   wire        clk,
+	input   wire        reset,
+
+	input   wire [31:0] data_i,
+
+	output  wire [31:0] edge_o
+
+);
+
+	// Write your logic here...
+	
+	
+endmodule
